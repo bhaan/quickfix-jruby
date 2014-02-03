e772ba2e934120ead2e83297d896cce0f4d20bb2
Brad Haan <bhaan@connamara.com>
2014-02-03 10:19:34 -0600

    added fx-connect jar

--- lib/quickfix.rb
+++ lib/quickfix.rb
@@ -1,6 +1,6 @@
 require 'java'
 
-require 'quickfixj-all-1.5.3.jar'
+require 'quickfixj-all-1.5.3-fx-connect.jar'
 require 'mina-core-1.1.7.jar'
 require 'slf4j-api-1.6.3.jar'
 require 'slf4j-jdk14-1.6.3.jar'
